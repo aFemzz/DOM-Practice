b30fd16b429e776c23ecc94b8c4ebf9a86882e2d
aFemzz <andhika.mailss@gmail.com>
2024-03-30 13:05:24 +0700

    feat : Added JS stuff

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 
 function SimulationPage(){
-    var isShowWrapper = document.getElementById("O-Wrapper")
-    console.log(isShowWrapper)
-    console.log(isShowWrapper.style.display)
+    var KreditMenuInitialStyle = document.getElementById("Kredit-Menu-Initial")
+    var MobilBaruStyle = document.getElementById("Mobil-Baru")
+    var BigNote = document.getElementById("big-note")
+    var SimulateButton = document.getElementById("Simulate-Button")
+    // console.log(isShowWrapper.style.display)
 
-    // isShowWrapper.style.display("none")
+    
 
-    if(isShowWrapper.style.display === "grid"){
-        isShowWrapper.style.display = "none"
-
-        // bikin element nya disini terus append ke <div class="Kredit-m">
-        
-        // let SimulationElement = document.createElement('div')
-        // document.createTextNode
-
+    if (KreditMenuInitialStyle.style.display === "inline"){
+        KreditMenuInitialStyle.style.display = "none"
+        MobilBaruStyle.style.display = "inline"
+        BigNote.style.display = "inline"
+        SimulateButton.style.display = "flex"
     }
 
-    // else {
-    //     isShowWrapper.style.display = "grid"
-    // }
+
 }
+
+function PembayaranUtama(){
+    var selectElementMobilBaru = document.getElementById("angsuran-mobil-baru")
+    var TotalPembayaranUtamaStyle = document.getElementById("Total-Pembayaran-Utama")
+    var AngsuranPerBulanStyle = document.getElementById("Angsuran-Perbulan")
+    var selectedValue = selectElementMobilBaru.value
+
+
+    
+    if(selectedValue === "Total-Pembayaran-Utama" ){
+        TotalPembayaranUtamaStyle.style.display = "flex"
+        // console.log("SELECTED!!")
+    }
+    if(selectedValue === "Pilih-Tipe-Perhitungan" ){
+        TotalPembayaranUtamaStyle.style.display = "none"
+        // console.log("SELECTED!!")
+    }
+    if(selectedValue === "Pilih-Tipe-Perhitungan" ){
+        TotalPembayaranUtamaStyle.style.display = "none"
+        // console.log("SELECTED!!")
+    }
+
+}
+
+
+
+function backToMainPage(){
+    var KreditMenuInitialStyle = document.getElementById("Kredit-Menu-Initial")
+    var MobilBaruStyle = document.getElementById("Mobil-Baru")
+    var BigNote = document.getElementById("big-note")
+
+    if (KreditMenuInitialStyle.style.display === "none"){
+        KreditMenuInitialStyle.style.display = "inline"
+        MobilBaruStyle.style.display = "none"
+        BigNote.style.display = "none"
+        SimulateButton.style.display = "none"
+    }
+}
+    
+
+
